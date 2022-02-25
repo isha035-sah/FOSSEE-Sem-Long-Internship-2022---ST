5ed9655d6de7568065c121bd8e395bbfcd551e26
Isha Sah <65812296+isha035-sah@users.noreply.github.com>
2022-02-25 19:49:54 +0545

    Add files via upload

---
+++ scr/java/TravlellerBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravlellerBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravlellerBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravlellerBlogApplication.class, args);
+	}
+
+}
